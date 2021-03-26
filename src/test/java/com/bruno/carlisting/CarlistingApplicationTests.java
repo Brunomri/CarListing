acf8ccb0bb4e7ae5b52f8a69cf9783149421957e
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-03-26 12:15:32 -0300

    Initial commit

---
+++ src/test/java/com/bruno/carlisting/CarlistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.carlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlistingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
