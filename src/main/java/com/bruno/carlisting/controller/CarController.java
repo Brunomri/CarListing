96ef381d3ed0049f3820a09799d9845fbe7ce7f1
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-03-31 10:05:08 -0300

    Create CarController.java findCarsByMake GET mapping

--- src/main/java/com/bruno/carlisting/controller/CarController.java
+++ src/main/java/com/bruno/carlisting/controller/CarController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,23 @@ public class CarController {
                                                  defaultValue = "10") int size) {
 
         Page<Car> carsPage = carService.getAllCars(page, size);
+        if(carsPage.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        else {
+            return ResponseEntity.ok().body(carsPage);
+        }
+
+    }
+
+    @GetMapping(value = "/{make}")
+    public ResponseEntity<Page<Car>> findCarsByMake(@PathVariable String make,
+                                                    @RequestParam(value = "page", required = false,
+                                                    defaultValue = "0") int page,
+                                                    @RequestParam(value = "size", required = false,
+                                                    defaultValue = "10") int size) {
+
+        Page<Car> carsPage = carService.getCarsByMake(make, page, size);
         if(carsPage.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
