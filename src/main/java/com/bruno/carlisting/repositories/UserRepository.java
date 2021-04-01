22b0d337c5b8be552f89e5bff70339da47a8caa2
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-04-01 17:19:54 -0300

    Add query in UserRepository.java to provide car_id and retrieve corresponding user_id

--- src/main/java/com/bruno/carlisting/repositories/UserRepository.java
+++ src/main/java/com/bruno/carlisting/repositories/UserRepository.java
@@ -2,10 +2,14 @@ package com.bruno.carlisting.repositories;
 
 import com.bruno.carlisting.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query(value = "SELECT user_id FROM car WHERE car_id = :searchCarId", nativeQuery = true)
+    Long searchUserByCarId(@Param("searchCarId") Long searchCarId);
 
 }
