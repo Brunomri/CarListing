22d18c760fa6834eb307ef11b3a7b4922ee7dcbf
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-04-01 10:51:05 -0300

    Change userService declaration to final

--- src/main/java/com/bruno/carlisting/controller/UserController.java
+++ src/main/java/com/bruno/carlisting/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
