381f42070aa6117dad9ae7d414809cd985b0cea3
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-04-07 16:57:05 -0300

    Update SwaggerConfig for API documentation
    Create getApiInfo method to display specific project information on SwaggerUI and custom responseMessageForGET method.

--- src/main/java/com/bruno/carlisting/config/SwaggerConfig.java
+++ src/main/java/com/bruno/carlisting/config/SwaggerConfig.java
@@ -2,11 +2,20 @@ package com.bruno.carlisting.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -15,8 +24,41 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.bruno.carlisting"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, responseMessageForGET())
+                .apiInfo(getApiInfo());
     }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Car Listing Application",
+                "Spring Boot demo project of a car listing application",
+                "1.0",
+                "https://github.com/Brunomri/CarListing",
+                new Contact("Brunomri","https://github.com/Brunomri/CarListing",
+                        "19270033+brunomri@users.noreply.github.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList()
+        );
+    }
+
+    private List<ResponseMessage> responseMessageForGET()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("500 message")
+//                    .responseModel(new ModelRef("Error"))
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(403)
+                    .message("Forbidden!")
+                    .build());
+        }};
+    }
+
 }
