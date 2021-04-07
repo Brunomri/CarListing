83f73eb605c7ea038dbf9c55c3a6682fbd2ee99e
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-04-07 17:02:35 -0300

    Update CarController for API documentation
    Add Swagger annotations to generate API information and description.

--- src/main/java/com/bruno/carlisting/controller/CarController.java
+++ src/main/java/com/bruno/carlisting/controller/CarController.java
@@ -2,6 +2,9 @@ package com.bruno.carlisting.controller;
 
 import com.bruno.carlisting.domain.Car;
 import com.bruno.carlisting.services.CarService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -30,7 +33,13 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping
+    @ApiOperation(value = "Return all cars grouped in pages")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return a page of cars"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Server exception"),
+    })
+    @GetMapping(produces = "application/json")
     public ResponseEntity<Page<Car>> findAllCars(@RequestParam(value = "page", required = false,
                                                  defaultValue = "0") int page,
                                                  @RequestParam(value = "size", required = false,
@@ -46,13 +55,25 @@ public class CarController {
 
     }
 
-    @GetMapping(value = "/{carId}")
+    @ApiOperation(value = "Find a car by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return the car with corresponding ID"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Server exception"),
+    })
+    @GetMapping(value = "/{carId}", produces = "application/json")
     public ResponseEntity<Car> findCarById(@PathVariable Long carId) {
         Car car = carService.getCarById(carId);
         return ResponseEntity.ok().body(car);
     }
 
-    @GetMapping(value = "/make/{make}")
+    @ApiOperation(value = "Find all cars by make")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return a page of cars from the corresponding make"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Server exception"),
+    })
+    @GetMapping(value = "/make/{make}", produces = "application/json")
     public ResponseEntity<Page<Car>> findCarsByMake(@PathVariable String make,
                                                     @RequestParam(value = "page", required = false,
                                                     defaultValue = "0") int page,
@@ -69,7 +90,13 @@ public class CarController {
 
     }
 
-    @GetMapping(value = "/users/{userId}")
+    @ApiOperation(value = "Find all cars by user")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return a page of cars added by a certain user"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Server exception"),
+    })
+    @GetMapping(value = "/users/{userId}", produces = "application/json")
     public ResponseEntity<Page<Car>> findCarsByUserId(@PathVariable Long userId,
                                         @RequestParam(value = "page", required = false, defaultValue = "0") int page,
                                         @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
@@ -84,7 +111,12 @@ public class CarController {
 
     }
 
-    @PostMapping(value = "/{userId}")
+    @ApiOperation(value = "Add a new car")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "New car created"),
+            @ApiResponse(code = 500, message = "Server exception"),
+    })
+    @PostMapping(value = "/{userId}", consumes = "application/json")
     public ResponseEntity<Car> createCar(@PathVariable Long userId, @Valid @RequestBody Car car) {
 
         Car newCar = carService.createCar(car, userId);
@@ -93,13 +125,23 @@ public class CarController {
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping(value = "/{carId}")
+    @ApiOperation(value = "Update an existing car")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Car updated"),
+            @ApiResponse(code = 500, message = "Server exception"),
+    })
+    @PutMapping(value = "/{carId}", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Car> updateCar(@PathVariable Long carId, @Valid @RequestBody Car car) {
 
         Car updatedCar = carService.updateCar(car, carId);
         return ResponseEntity.ok().body(updatedCar);
     }
 
+    @ApiOperation(value = "Delete an existing car")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Car deleted"),
+            @ApiResponse(code = 500, message = "Server exception"),
+    })
     @DeleteMapping(value = "/{carId}")
     public ResponseEntity<Void> deleteCar(@PathVariable Long carId) {
 
