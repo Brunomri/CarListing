66535058ea46d6fbd497b836b37a62e9287c9dad
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-05-20 15:23:22 -0300

    Create ErrorHandlingControllerAdvice, ValidationErrorResponse and Violation
    Handle exception for all controller classes.

---
+++ src/main/java/com/bruno/carlisting/exceptions/ValidationErrorResponse.java
@@ -0,0 +1,17 @@
+package com.bruno.carlisting.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErrorResponse {
+
+    private List<Violation> violations = new ArrayList<>();
+
+    public List<Violation> getViolations() {
+        return violations;
+    }
+
+    public void setViolations(List<Violation> violations) {
+        this.violations = violations;
+    }
+}
