2c9c1df3029e22ef074552b306dcf18d358ef48c
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-05-20 10:21:30 -0300

    Update response status to 404

--- src/main/java/com/bruno/carlisting/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/bruno/carlisting/exceptions/ObjectNotFoundException.java
@@ -3,7 +3,7 @@ package com.bruno.carlisting.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ObjectNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
