7a1f989c773ddd596c2254a4ff03b276f5140c36
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-05-25 15:54:50 -0300

    Improve unit tests

--- src/test/java/com/bruno/carlisting/services/UserServiceTests.java
+++ src/test/java/com/bruno/carlisting/services/UserServiceTests.java
@@ -5,12 +5,14 @@ import com.bruno.carlisting.domain.User;
 import com.bruno.carlisting.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +20,9 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
@@ -30,6 +34,12 @@ public class UserServiceTests {
 
     @Mock
     private RoleService mockedRoleService;
+
+    @Mock
+    private PagingService mockedPagingService;
+
+    @InjectMocks
+    private UserServiceImpl userService;
 
     @Test
     public void retrieveAllExistingUsers() {
@@ -45,9 +55,9 @@ public class UserServiceTests {
         Page<User> usersPage = new PageImpl<>(newUsers);
         when(mockedUserRepository.findAll(pageRequest)).thenReturn(usersPage);
 
-        UserService userService = new UserServiceImpl(mockedUserRepository, mockedRoleService);
         assertEquals(3, userService.getAllUsers(0, 3).getTotalElements());
-
+        verify(mockedUserRepository, times(1)).findAll(any(Pageable.class));
+        verify(mockedPagingService, times(1)).validatePage(any(Page.class));
     }
 
     @Test
@@ -56,9 +66,9 @@ public class UserServiceTests {
 //        user.get().setUserId(123456L);
         when(mockedUserRepository.findById(123456L)).thenReturn(user);
 
-        UserService userService = new UserServiceImpl(mockedUserRepository, mockedRoleService);
         assertEquals(user.get(), userService.getUserById(123456L));
-
+        verify(mockedUserRepository, times(1)).findById(anyLong());
+        verifyNoInteractions(mockedPagingService);
     }
 
     @Test
@@ -69,23 +79,27 @@ public class UserServiceTests {
         when(mockedUserRepository.searchUserByCarId(987654L)).thenReturn(123456L);
         when(mockedUserRepository.findById(123456L)).thenReturn(user);
 
-        UserService userService = new UserServiceImpl(mockedUserRepository, mockedRoleService);
         assertEquals(user.get(), userService.getUserByCarId(987654L));
-
+        verify(mockedUserRepository, times(1)).searchUserByCarId(anyLong());
+        verify(mockedUserRepository, times(1)).findById(anyLong());
+        verifyNoInteractions(mockedPagingService);
     }
 
     @Test
     public void createUser() {
         User user = new User();
 //        user.setUserId(1L);
+//        ReflectionTestUtils.setField(user, "userId", 1L);
 
         List<Integer> userRoles = new ArrayList<>();
 
         Optional<Role> role1 = Optional.of(new Role());
 //        role1.get().setRoleId(1);
+        ReflectionTestUtils.setField(role1.get(), "roleId", 1);
 
         Optional<Role> role2 = Optional.of(new Role());
 //        role2.get().setRoleId(2);
+        ReflectionTestUtils.setField(role2.get(), "roleId", 2);
 
         userRoles.add(role1.get().getRoleId());
         userRoles.add(role2.get().getRoleId());
@@ -94,12 +108,12 @@ public class UserServiceTests {
         when(mockedRoleService.getRoleById(2)).thenReturn(role2.get());
         when(mockedUserRepository.save(user)).thenReturn(user);
 
-        UserService userService = new UserServiceImpl(mockedUserRepository, mockedRoleService);
         User newUser = userService.createUser(user, userRoles);
         assertNotNull(newUser);
         assertEquals(role1.get().getRoleId(), newUser.getRoles().get(0).getRoleId());
         assertEquals(role2.get().getRoleId(), newUser.getRoles().get(1).getRoleId());
-
+        verify(mockedRoleService, times(2)).getRoleById(anyInt());
+        verify(mockedUserRepository, times(1)).save(any(User.class));
+        verifyNoInteractions(mockedPagingService);
     }
-
 }
