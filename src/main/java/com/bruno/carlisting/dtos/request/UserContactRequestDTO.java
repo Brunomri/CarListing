798d4b1a1055eebfbf3a0b09b5e08f59f02cf2de
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-05-31 11:43:13 -0300

    Create DTO for request to update User contact

---
+++ src/main/java/com/bruno/carlisting/dtos/request/UserContactRequestDTO.java
@@ -0,0 +1,15 @@
+package com.bruno.carlisting.dtos.request;
+
+import lombok.Getter;
+
+import javax.validation.constraints.Pattern;
+
+@Getter
+public class UserContactRequestDTO {
+
+    private static final long serialVersionUID = 1L;
+
+    @Pattern(regexp = "^[\\w.-]+@[\\w-]+[.]com$",
+            message = "Contact must be an e-mail address in format: localpart@domain.com")
+    private String contact;
+}
