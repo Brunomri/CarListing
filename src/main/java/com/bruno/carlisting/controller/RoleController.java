5126c405da2935af42a904252d787c1672248a48
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-06-10 11:24:18 -0300

    Create PUT request to update Role

--- src/main/java/com/bruno/carlisting/controller/RoleController.java
+++ src/main/java/com/bruno/carlisting/controller/RoleController.java
@@ -1,6 +1,9 @@
 package com.bruno.carlisting.controller;
 
 import com.bruno.carlisting.domain.Role;
+import com.bruno.carlisting.dtos.request.role.RoleRequestDTO;
+import com.bruno.carlisting.dtos.response.role.RolePrivateResponseDTO;
+import com.bruno.carlisting.dtos.response.role.RolePublicResponseDTO;
 import com.bruno.carlisting.services.interfaces.RoleService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +27,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
-import java.net.URI;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -52,7 +55,7 @@ public class RoleController {
         @ApiResponse(code = 500, message = "Server exception"),
     })
     @GetMapping(value = "/all", produces = "application/json")
-    public ResponseEntity<Page<Role>> findAllRoles(
+    public ResponseEntity<Page<RolePublicResponseDTO>> findAllRoles(
 
         @RequestParam(value = "page", required = false, defaultValue = ROLE_PAGE_DEFAULT_NUMBER)
         @Min(value = ROLE_PAGE_MIN_NUMBER,
@@ -64,8 +67,8 @@ public class RoleController {
         @Max(value = ROLE_PAGE_MAX_SIZE,
             message = "Page size must be less than or equal to " + ROLE_PAGE_MAX_SIZE) int size) {
 
-        Page<Role> rolesPage = roleService.getAllRoles(page, size);
-        return ResponseEntity.ok().body(rolesPage);
+        var rolesPageDTO = RolePublicResponseDTO.toRolesPagePublicDTO(roleService.getAllRoles(page, size));
+        return ResponseEntity.ok().body(rolesPageDTO);
     }
 
     @ApiOperation(value = "Find a role by ID")
@@ -80,7 +83,7 @@ public class RoleController {
 
         @PathVariable @Positive(message = "Role ID must be a positive integer") Integer roleId) {
 
-        Role role = roleService.getRoleById(roleId);
+        var role = roleService.getRoleById(roleId);
         return ResponseEntity.ok().body(role);
     }
 
@@ -108,12 +111,30 @@ public class RoleController {
     @PostMapping(consumes = "application/json")
     public ResponseEntity<Role> createRole(
 
-        @Valid @RequestBody Role role) {
+        @Valid @RequestBody RoleRequestDTO roleRequestDTO) {
 
-        Role newRole = roleService.createRole(role);
-        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/roles/{roleId}").
+        var newRole = roleService.createRole(roleRequestDTO.toRole());
+        var uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/roles/{roleId}").
                 buildAndExpand(newRole.getRoleId()).toUri();
-        return ResponseEntity.created(uri).build();
+        return ResponseEntity.created(uri).body(newRole);
+    }
+
+    @ApiOperation(value = "Update an existing role")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Role updated"),
+        @ApiResponse(code = 400, message = "Invalid Role data provided"),
+        @ApiResponse(code = 404, message = "Role not found"),
+        @ApiResponse(code = 500, message = "Server exception")
+    })
+    @PutMapping(value = "/{roleId}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<RolePrivateResponseDTO> updateRole(
+
+            @PathVariable @Positive(message = "Role ID must be a positive integer") Integer roleId,
+
+            @Valid @RequestBody RoleRequestDTO roleRequestDTO) {
+
+        var updatedRole = roleService.updateRole(roleRequestDTO.toRole(), roleId);
+        return ResponseEntity.ok().body(RolePrivateResponseDTO.toRolePrivateResponseDTO(updatedRole));
     }
 
     @ApiOperation(value = "Delete an existing role")
