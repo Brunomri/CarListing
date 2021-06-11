7e76ae933a49df5be0f19a07fb86605f10a5b213
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-06-11 14:01:43 -0300

    Create inheritance from RolePublicResponseDTO to RolePrivateResponseDTO and UserAdminResponseDTO

--- src/main/java/com/bruno/carlisting/dtos/response/role/RolePrivateResponseDTO.java
+++ src/main/java/com/bruno/carlisting/dtos/response/role/RolePrivateResponseDTO.java
@@ -1,26 +1,26 @@
 package com.bruno.carlisting.dtos.response.role;
 
 import com.bruno.carlisting.domain.Role;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-public class RolePrivateResponseDTO {
-
-    private static final long serialVersionUID = 1L;
+public class RolePrivateResponseDTO extends RolePublicResponseDTO {
 
     private Integer roleId;
-    private String type;
     private List<Long> usersIds;
+
+    public RolePrivateResponseDTO(String type, Integer roleId, List<Long> usersIds) {
+        super(type);
+        this.roleId = roleId;
+        this.usersIds = usersIds;
+    }
 
     public static RolePrivateResponseDTO toRolePrivateResponseDTO(Role role) {
         List<Long> usersIds = new ArrayList<>();
         role.getUsers().forEach(user -> usersIds.add(user.getUserId()));
-        return new RolePrivateResponseDTO(role.getRoleId(), role.getType(), usersIds);
+        return new RolePrivateResponseDTO(role.getType(), role.getRoleId(), usersIds);
     }
 }
