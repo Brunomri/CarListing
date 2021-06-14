afb3ad2efbb44f6861cf4ec2f1f79a616fc3ec4c
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-06-14 11:19:52 -0300

    Remove commented code

--- src/main/java/com/bruno/carlisting/repositories/CarRepository.java
+++ src/main/java/com/bruno/carlisting/repositories/CarRepository.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 
-//    @Query(value = "SELECT * FROM car WHERE make = :searchMake", nativeQuery = true)
-//    Page<Car> searchCarsByMake(@Param("searchMake") String searchMake, Pageable pageable);
     Page<Car> findByMake(String make, Pageable pageRequest);
 
     Page<Car> findByUser_UserId(Long userId, Pageable pageRequest);
