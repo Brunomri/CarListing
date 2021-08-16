f28107c33a3c54eb997c59d0e69f76eae81a095c
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-08-16 09:57:05 -0300

    Add logging statements

--- src/main/java/com/bruno/carlisting/services/implementations/PagingServiceImpl.java
+++ src/main/java/com/bruno/carlisting/services/implementations/PagingServiceImpl.java
@@ -2,18 +2,26 @@ package com.bruno.carlisting.services.implementations;
 
 import com.bruno.carlisting.exceptions.ObjectNotFoundException;
 import com.bruno.carlisting.services.interfaces.PagingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class PagingServiceImpl implements PagingService {
 
     @Override
     public void validatePage(Page<?> page, String message) {
-        if (page.getTotalElements() == 0) throw new ObjectNotFoundException(
-                String.format("%s, and no entries were found in the database", message));
-        else if (!page.hasContent()) throw new ObjectNotFoundException(
-                String.format("%s, however there are %s entries up to page %s",
-                        message, page.getTotalElements(), page.getTotalPages() - 1));
+        if (page.getTotalElements() == 0) {
+            log.warn("Object not found exception occurred:");
+            throw new ObjectNotFoundException(
+                    String.format("%s, and no entries were found in the database", message));
+        }
+        else if (!page.hasContent()) {
+            log.warn("Object not found exception occurred:");
+            throw new ObjectNotFoundException(
+                    String.format("%s, however there are %s entries up to page %s",
+                            message, page.getTotalElements(), page.getTotalPages() - 1));
+        }
     }
 }
